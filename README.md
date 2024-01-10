c83541849b2a004c124ff4f0d393923bf1a62b78
IaraTassi <iaratassi@hotmail.com>
2024-01-10 14:58:51 -0300

    projeto lógica concluida, falta o css

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-git
+<h1>
+    Projeto Lista de Tarefas
+</h1>
+
+Repositório desenvolvido para fins didáticos, desenvolvido no curso Front-end em React aula 22 com professor Wallison Silva da [Ada Tech](https://ada.tech/).
+
+## Objetivo
+
+Aplicar o conhecimentos das aulas de HTML, CSS, JavaScript, TypeScript, POO, e Browser APIs.
+
+## Ferramentas
+
+[![GitHub](https://img.shields.io/badge/GitHub-000?style=for-the-badge&logo=github&logoColor=30A3DC)](https://docs.github.com/)
+[![Git](https://img.shields.io/badge/Git-000?style=for-the-badge&logo=git&logoColor=E94D5F)](https://git-scm.com/doc)
+[![HTML](https://img.shields.io/badge/HTML5-000?style=for-the-badge&logo=html5&logoColor=orange)](https://git-scm.com/doc)
+[![CSS](https://img.shields.io/badge/CSS3-000?style=for-the-badge&logo=css3&logoColor=blue)](https://git-scm.com/doc)
+[![JavaScript](https://img.shields.io/badge/JavaScript-000?style=for-the-badge&logo=javascript&logoColor=F7DF1E)](https://git-scm.com/doc)
--- index.js
+++ index.js
@@ -4,6 +4,79 @@ const todoListUl = document.querySelector("#todo-list");
 
 let tasks = [];
 
+function renderTaskHtml(taskTitle, done = false) {
+  // Adiconando a nova tarefa no HTML
+  const li = document.createElement("li");
+
+  const input = document.createElement("input"); // Criando input
+  input.setAttribute("type", "checkbox");
+  input.addEventListener("change", (event) => {
+    const liToTogle = event.target.parentElement;
+
+    const spanToTogle = liToTogle.querySelector("span");
+
+    const done = event.target.checked;
+    if (done) {
+      spanToTogle.style.textDecoration = "line-through";
+      spanToTogle.style.textDecoration = "none";
+    }
+
+    // Alterar o done
+    tasks = tasks.map((t) => {
+      if (t.title === spanToTogle.textContent) {
+        return {
+          title: t.title,
+          done: !t.done,
+        };
+      }
+
+      return t;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  input.checked = done;
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+  if (done) {
+    span.style.textDecoration = "line-through";
+  }
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+  button.addEventListener("click", (event) => {
+    const liToRemove = event.target.parentElement;
+
+    const titleToRemove = liToRemove.querySelector("span").textContent;
+
+    tasks = tasks.filter((t) => t.title !== titleToRemove);
+
+    todoListUl.removeChild(liToRemove); // Tenho elemento pai que disparou o envento e removo o filho com o parentElement
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoListUl.appendChild(li); //Adicionar um filho dentro de uma tag li dentro ul
+}
+
+// Assim que a página carregar preciso colocar elas no HTML
+window.onload = () => {
+  const tasksOnLocalStorage = localStorage.getItem("tasks");
+
+  if (!tasksOnLocalStorage) return;
+
+  tasks = JSON.parse(tasksOnLocalStorage);
+
+  tasks.forEach((t) => {
+    renderTaskHtml(t.title, t.done);
+  });
+  console.log(tasksOnLocalStorage);
+};
+
 // Evento para a página não carregar ao submeter o formulário
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -18,16 +91,15 @@ form.addEventListener("submit", (event) => {
   }
 
   // Adicionando a nova tarefa no array de tasks
-  tasks.push(taskTitle);
+  tasks.push({
+    title: taskTitle,
+    done: false,
+  });
+  // Salvando no localStorage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  // Adiconando a nova tarefa no HTML
-  const li = document.createElement("li"); // Criando tag li dentro do JS
-
-  const input = document;
-
-  //li.textContent = taskTitle; // Criando tag <li>Tarefa 2</li> com o conteúdo em texto
-
-  todoListUl.appendChild(li); //Adicionar um filho dentro de uma tag
+  // Chamo a função
+  renderTaskHtml(taskTitle);
 
   // Limpar o input
   taskTitleInput.value = "";
